c9db37dc96508058aec485e688dea78120c88619
Simon Vandereecken <simon@nithou.net>
2013-03-02 17:49:59 +0100

    Remove Jquery call

--- functions.php
+++ functions.php
@@ -62,31 +62,7 @@ function theme_comments($comment, $args, $depth) {
 
 /************* ENQUEUE JS *************************/
 
-/* pull jquery from google's CDN. If it's not available, grab the local copy. Code from wp.tutsplus.com :-) */
-
-$url = 'http://code.jquery.com/jquery-latest.min.js'; // the URL to check against  
-$test_url = @fopen($url,'r'); // test parameters  
-if( $test_url !== false ) { // test if the URL exists  
-
-    function load_external_jQuery() { // load external file  
-        wp_deregister_script( 'jquery' ); // deregisters the default WordPress jQuery  
-        wp_register_script('jquery', 'http://code.jquery.com/jquery-latest.min.js'); // register the external file  
-        wp_enqueue_script('jquery'); // enqueue the external file  
-    }  
-
-    add_action('wp_enqueue_scripts', 'load_external_jQuery'); // initiate the function  
-} else {  
-
-    function load_local_jQuery() {  
-        wp_deregister_script('jquery'); // initiate the function  
-        wp_register_script('jquery','http://assets.nithou.net/scripts/jquery.min.js', __FILE__, false, '1.7.2', true); // register the local file  
-        wp_enqueue_script('jquery'); // enqueue the local file  
-    }  
-
-    add_action('wp_enqueue_scripts', 'load_local_jQuery'); // initiate the function  
-}  
-
-/* LOad Foundation Scripts */
+/* Load Foundation Scripts */
 
 function foundation_js(){
     wp_register_script( 'foundation-reveal', 'http://assets.nithou.net/scripts/fnd4/foundation/foundation.alerts.js' ); 
