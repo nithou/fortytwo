2ebdb5adab10163e9df69a0a9fda3c00a0bff4cb
Simon Vandereecken <simon@nithou.net>
2013-07-30 10:09:26 +0200

    Fixed viewport typo

--- header.php
+++ header.php
@@ -11,7 +11,8 @@
   <meta charset="<?php bloginfo('charset'); ?>">
 
   <!-- Set the viewport width to device width for mobile -->
-  <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
+
 
   <title><?php wp_title('|',true,'right'); ?><?php bloginfo('name'); ?></title>
   
